52f86400b70a4f527296e1f4e25b525387dc3355
Kaustubh1204 <kaustubhmishra350@gmail.com>
2026-03-08 06:24:41 +0530

    authentication and device flow

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,36 @@
 import express from "express";
-
+import dotenv from "dotenv";
+dotenv.config();
+import { auth } from "./lib/auth.js";
+import cors from "cors";
+import { toNodeHandler } from "better-auth/node";
+import { fromNodeHeaders } from "better-auth/node";
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Replace with your frontend's origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  })
+);
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+
+
+app.get("/api/me", async (req, res) => {
+  const session = await auth.api.getSession({
+    headers: fromNodeHeaders(req.headers),
+  })
+  res.json(session)
+})
+
+app.get("/devive", async (req, res) => {
+  const {user_code} = req.query;
+  res.redirect(`http://localhost:3000/device?user_code=${user_code}`)
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`your app is running on http://localhost:${process.env.PORT}`)
+})
